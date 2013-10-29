acc367fba1465b3db9d0925657c70dd6498fe542
Joseph Ellyson <joseph.ellyson@rackspace.com>
2013-10-29 17:46:59 +0000

    got past the csrf token issue.

--- rackerbooksite/socialapp/views.py
+++ rackerbooksite/socialapp/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 from django.template.loader import get_template
 from django.template import Context
+from django.template import RequestContext
 from django.http import HttpResponse
+from django.contrib import auth
 from django.core.context_processors import csrf
 from django.template.defaulttags import csrf_token
 from models import *
@@ -28,8 +30,11 @@ def landing(request):
         event_user_dict[event_user.event.event_id]["users"].append(event_user.user)
         event_user_dict[event_user.event.event_id]["count"] += 1
         
-    html = t.render(Context({'all_activity_groups' : activity_groups, 
-                             'all_sports_groups': sports_groups, 'discussionThreads': discussionThreads, 'event_user_dict': event_user_dict}),
+    landingInfo = {'all_activity_groups' : activity_groups, 
+                             'all_sports_groups': sports_groups, 'discussionThreads': discussionThreads, 'event_user_dict': event_user_dict,
+                             'csrf_token': csrf_token}
+    landingInfo.update(csrf(request))                             
+    html = t.render(Context(landingInfo),
                             )
     return HttpResponse(html)
 	
@@ -42,17 +47,26 @@ def createGroup(request):
 	t = get_template('createGroup.html')
 	html = t.render(Context({}))
 	return HttpResponse(html)
-	
+    
 def loggedIn(request):
-    Email = ''
-    Password = ''
-    t = get_template('loggedIn.html')
-    user = Users.objects.get(firstName="Joseph")
-    html = t.render(Context({'firstName': user.firstName, 
-                             'lastName': user.lastName, 
-                              'Email': Email, 
-                              'Password': Password}))
-    return HttpResponse(html)
+    if request.method != 'POST':
+        raise Http404('Only POSTs are allowed')
+    
+    try :
+        user = Users.objects.get(email=request.POST['Email'])
+        if user.password == request.POST['Password']:
+            request.session['user_id'] = user.user_id
+            Email = request.POST['Email']
+            Password = request.POST['Password']
+            loginInfo = {"Email": Email, "Password": Password,
+                                        "firstName": user.firstName, "lastName":
+                                        user.lastName}
+            loginInfo.update(csrf(request))
+            t = get_template('loggedIn.html')
+            html = t.render(Context(loginInfo))
+            return HttpResponse(html)
+    except user.DoesNotExist:
+        return HttpResponse("Your email and password didn't match")
 
 def editAccount(request):
 	t = get_template('editAccount.html')
@@ -73,6 +87,9 @@ def profile(request):
 	t = get_template('profile.html')
 	html = t.render(Context({}))
 	return HttpResponse(html)
-	
+    
+def csrf_failure(request, reason=""):
+    ctx = {'reason': reason}
+    return render_to_response("failure.html", ctx)
 
 	
